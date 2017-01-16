9c6ca6a71c0aeb3a2ee50b1d84e87405b61aefa6
pers1307 <pers1307>
2017-01-16 14:37:58 +0500

    lesson 5

---
+++ lesson5.js
@@ -0,0 +1,26 @@
+console.log('Some "long" string');
+console.log('Some "long" string'.length);
+
+console.log("Another \
+very long \
+string");
+
+console.log("Another \n\tvery long \n\t\tstring");
+
+console.log("Another \"very \\ long\" string");
+
+console.log("Hello" + " yellow");
+
+var string = 'Sometimes the same is different';
+console.log(string.charAt(0));
+console.log(string.substring(10));
+console.log(string.substring(10, 21));
+
+console.log(string.slice(-10));
+console.log(string.indexOf('i'));
+console.log(string.split(' '));
+
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+console.log(string[5]);
