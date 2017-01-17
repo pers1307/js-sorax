e90ed435642642e4013611ef484f12b11fe792d3
pers1307 <pers1307>
2017-01-17 10:42:04 +0500

    lesson 9

---
+++ lesson9.js
@@ -0,0 +1,15 @@
+var name = 'Sorax';
+
+if (name === 'Sorax') {
+
+    console.log('Sorax');
+} else if (name === 'pers1307') {
+
+    console.log('pers1307');
+}
+
+var x = 15;
+// Тернарный оператор
+var text = x > 10 ? 'х больше десяти' : 'х меньше или равен десяти';
+
+console.log(text);
