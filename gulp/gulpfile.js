887a388fecd8d9c6d1db795406acf012faf5d378
pers1307 <pers1307>
2017-02-02 11:07:10 +0500

    gulp complite

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,12 +1,16 @@
+'use strict';
+
 var gulp = require('gulp'),
     concatCSS = require('gulp-concat-css'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    prefix = require('gulp-autoprefixer');
 
 gulp.task('default', function () {
     return gulp.src('css/*.css')
         .pipe(concatCSS("bundle.css"))
+        .pipe(prefix('last 2 versions', '> 1%', 'ie 9'))
         .pipe(minifyCSS())
         .pipe(rename("bundle.min.css"))
         .pipe(gulp.dest('app/'))
